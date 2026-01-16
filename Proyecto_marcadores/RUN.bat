034b10ea6470a1b918c09c91a1aadc8962204c48
Sebastian Omar de la Vega <sebastianomardelavega@gmail.com>
2026-01-15 21:01:07 -0300

    Muevo Proyecto_marcadores a repositorio independiente

--- Proyecto_marcadores/RUN.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Esto asegura que el script se ejecute en la carpeta donde esta el .bat
-cd /d "%~dp0"
-
-echo Lanzando Organizador de Marcadores...
-:: Usamos el python del entorno virtual directamente
-".\venv\Scripts\python.exe" Launcher.py
-
-pause
